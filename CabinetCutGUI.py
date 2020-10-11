e6448785f97cb979172c7677760f9310506156ad
Sam Quiring <lafcrules@gmail.com>
2020-10-11 16:39:36 -0700

    Update CabinetCutGUI.py

--- CabinetCutGUI.py
+++ CabinetCutGUI.py
@@ -1,11 +1,19 @@
 from tkinter import *
 from tkinter import filedialog
 from cabinets import *
-   
+
+# created by Sam Quiring
+#CabinetCutGUI is the GUI for the cabinet cut calculator
+#this allows for non-python experienced users such as my
+#father to use this program with relative ease after the
+#dependencies are installed
+
 # Function for opening the  
 # file explorer window 
 input_file = ""
 output_file = ""
+
+#this is the first browseFiles which is for the input file
 def browseFiles1(): 
     global input_file
     filename = filedialog.askopenfilename(initialdir = "/", 
@@ -14,10 +22,10 @@ def browseFiles1():
                                                         ".xlsx"), 
                                                        ("excel files", 
                                                         ".xls")))      
-    # Change label contents 
     label_input.configure(text="File Opened: "+filename)
     input_file = str(filename) 
 
+#this is the second browseFiles which is for the output file
 def browseFiles2(): 
     global output_file
     filename = filedialog.askopenfilename(initialdir = "/", 
@@ -26,15 +34,17 @@ def browseFiles2():
                                                         ".xlsx"), 
                                                        ("excel files", 
                                                         ".xls")))      
-    # Change label contents 
     label_output.configure(text="File Opened: "+filename)
     output_file = str(filename)
 
+#when this function is run it runs the CabinetCutCalculator and then
+#prints out "run successful", if it encountered no errors
 def run_cabinets():
     csvConverter(input_file, output_file)
     run = Label(text = "run successful, please check your output file")
     run.pack()
 
+#this is the heart of the GUI which was setup using Tkinter
 window = Tk()
 window.title("Cabinet Cut Calculator")
 title = Label(text = "Sam's Automatic Cabinet Cut Calculator")
